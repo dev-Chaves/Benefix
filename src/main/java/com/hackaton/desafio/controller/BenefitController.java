aab3e9dbecca06416116b0dbb326b457b9015223
chaves <chavesprogrammer@gmail.com>
2025-05-17 10:15:06 -0300

    Benefit Controller Basic

--- src/main/java/com/hackaton/desafio/controller/BenefitController.java
+++ src/main/java/com/hackaton/desafio/controller/BenefitController.java
@@ -1,13 +1,26 @@
 package com.hackaton.desafio.controller;
 
 import com.hackaton.desafio.repository.BenefitRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("teste/benefits")
-public class BenefitsController {
+public class BenefitController {
 
     private final BenefitRepository benefitsRepository;
+    private final BenefitService benefitService;
+
+    public BenefitController(BenefitRepository benefitRepository, BenefitService benefitService) {
+        this.benefitsRepository = benefitRepository;
+        this.benefitService = benefitService;
+    }
+
+    @GetMapping("benefits")
+    public ResponseEntity<?> getBenefits() {
+        return benefitService.getBenefits());
+    }
 
 }

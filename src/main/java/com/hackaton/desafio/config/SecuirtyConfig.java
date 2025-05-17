f69e0928803738c6838309f56f3809f33ca2280b
chaves <chavesprogrammer@gmail.com>
2025-05-17 08:28:55 -0300

    add SecurityFilter and SecurityConfig basic

--- src/main/java/com/hackaton/desafio/config/SecuirtyConfig.java
+++ src/main/java/com/hackaton/desafio/config/SecuirtyConfig.java
@@ -1,4 +1,38 @@
 package com.hackaton.desafio.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
 public class SecuirtyConfig {
+
+    private final CustomUserDetailsService customUserDetailsService;
+    private final SecurityFilter securityFilter;
+
+    public SecuirtyConfig(CustomUserDetailsService customUserDetailsService, SecurityFilter securityFilter) {
+        this.customUserDetailsService = customUserDetailsService;
+        this.securityFilter = securityFilter;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        http
+                .csrf(csrf->csrf.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/teste/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
+
+        return http.build();
+    }
+
 }
+1

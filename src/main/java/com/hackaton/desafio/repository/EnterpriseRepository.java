3ea6f21fa9b25de0a2a10cbb8efe888e8b56d39f
dev-Chaves <chavesprogrammer@gmail.com>
2025-05-16 22:47:50 -0300

    add @Repository on repositories

--- src/main/java/com/hackaton/desafio/repository/EnterpriseRepository.java
+++ src/main/java/com/hackaton/desafio/repository/EnterpriseRepository.java
@@ -2,6 +2,8 @@ package com.hackaton.desafio.repository;
 
 import com.hackaton.desafio.entity.EnterpriseEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface EnterpriseRepository extends JpaRepository<EnterpriseEntity, Long> {
 }
--- src/main/java/com/hackaton/desafio/repository/PartnershipRepository.java
+++ src/main/java/com/hackaton/desafio/repository/PartnershipRepository.java
@@ -2,6 +2,8 @@ package com.hackaton.desafio.repository;
 
 import com.hackaton.desafio.entity.PartnershipEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PartnershipRepository extends JpaRepository<PartnershipEntity, Long> {
 }

b4e129bf020a0537fa2e22c07eb9f3d2cf65361e
chaves <chavesprogrammer@gmail.com>
2025-05-17 08:45:43 -0300

    USerDetails created for validate

---
+++ src/main/java/com/hackaton/desafio/config/CustomUserDetailsService.java
@@ -0,0 +1,35 @@
+package com.hackaton.desafio.config;
+
+import com.hackaton.desafio.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.ArrayList;
+
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        if(username == null || username.isEmpty()){
+            throw new UsernameNotFoundException("Username cannot be null or empty");
+        }
+        var user = userRepository.findByName(username);
+        if(user == null){
+            throw new UsernameNotFoundException("User not found");
+        }
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getName(),
+                user.getPassword(),
+                new ArrayList<>()
+        );
+    }
+}

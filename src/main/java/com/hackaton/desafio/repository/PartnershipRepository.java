405dea2ada7b0cc01f53b6474f4a05152e910e99
dev-Chaves <chavesprogrammer@gmail.com>
2025-05-17 19:34:07 -0300

    create a querie for search Partnership

--- src/main/java/com/hackaton/desafio/repository/PartnershipRepository.java
+++ src/main/java/com/hackaton/desafio/repository/PartnershipRepository.java
@@ -1,9 +1,13 @@
 package com.hackaton.desafio.repository;
 
+import com.hackaton.desafio.entity.EnterpriseEntity;
 import com.hackaton.desafio.entity.PartnershipEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PartnershipRepository extends JpaRepository<PartnershipEntity, Long> {
+
+    boolean existsBySupplierEnterpriseAndConsumerEnterprise(EnterpriseEntity supplier, EnterpriseEntity consumer);
+
 }

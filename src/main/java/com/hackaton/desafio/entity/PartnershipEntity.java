6189016b621572ab28e511a7feebcdc6254ff98c
dev-Chaves <chavesprogrammer@gmail.com>
2025-05-16 22:05:46 -0300

    partnership entity mapped

--- src/main/java/com/hackaton/desafio/entity/PartnershipEntity.java
+++ src/main/java/com/hackaton/desafio/entity/PartnershipEntity.java
@@ -1,4 +1,21 @@
 package com.hackaton.desafio.entity;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_partnership")
 public class PartnershipEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "consumer_enterprise_id", nullable = false)
+    private EnterpriseEntity consumerEnterprise;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "supplier_enterprise_id", nullable = false)
+    private EnterpriseEntity supplierEnterprise;
+
 }

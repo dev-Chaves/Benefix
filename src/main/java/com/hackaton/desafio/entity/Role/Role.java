c2015325a3422a25cd458cae1264ed42d058b3f0
dev-Chaves <chavesprogrammer@gmail.com>
2025-05-16 22:05:59 -0300

    role of Users

--- src/main/java/com/hackaton/desafio/entity/Role/Role.java
+++ src/main/java/com/hackaton/desafio/entity/Role/Role.java
@@ -1,4 +1,20 @@
 package com.hackaton.desafio.entity.Role;
 
 public enum Role {
+    ADMIN("ADMIN"),
+    USER("USER");
+
+    private String role;
+
+    Role(String role) {
+        this.role = role;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
 }

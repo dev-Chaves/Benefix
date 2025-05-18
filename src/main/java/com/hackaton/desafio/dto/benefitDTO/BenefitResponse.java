bbec2ee4f0fbd625344c578abea3b1a68ac503d2
dev-Chaves <chavesprogrammer@gmail.com>
2025-05-18 13:55:55 -0300

    adjustment for benefits response

--- src/main/java/com/hackaton/desafio/dto/benefitDTO/BenefitResponse.java
+++ src/main/java/com/hackaton/desafio/dto/benefitDTO/BenefitResponse.java
@@ -1,6 +1,7 @@
 package com.hackaton.desafio.dto.benefitDTO;
 
 public record BenefitResponse(
+        Long id,
         String description,
         String nameSupplierEnterprise
 ) {
--- src/main/java/com/hackaton/desafio/services/BenefitService.java
+++ src/main/java/com/hackaton/desafio/services/BenefitService.java
@@ -1,9 +1,20 @@
 package com.hackaton.desafio.services;
 
+import com.hackaton.desafio.dto.benefitDTO.BenefitResponse;
+import com.hackaton.desafio.entity.BenefitEntity;
+import com.hackaton.desafio.entity.EnterpriseEntity;
+import com.hackaton.desafio.entity.UserEntity;
 import com.hackaton.desafio.repository.BenefitRepository;
 import com.hackaton.desafio.repository.EnterpriseRepository;
 import com.hackaton.desafio.repository.UserRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class BenefitService {
@@ -17,4 +28,26 @@ public class BenefitService {
         this.userRepository = userRepository;
         this.enterpriseRepository = enterpriseRepository;
     }
+
+    public ResponseEntity<?> getBenefitsByEnterprise() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication == null || !(authentication.getPrincipal() instanceof String username)) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("User not authenticated");
+        }
+
+        UserEntity user = userRepository.findByName(username).orElseThrow(()->
+                new UsernameNotFoundException("User not found"));
+
+        EnterpriseEntity enterprise = user.getEnterprise();
+
+        List<BenefitEntity> benefits = benefitRepository.findByEnterpriseId(user.getEnterprise().getId());
+
+        List<BenefitResponse> response = benefits.stream()
+                .map(b -> new BenefitResponse(b.getId(), b.getDescription(), b.getSupplierEnterprise().getEnterprise()))
+                .toList();
+
+        return ResponseEntity.ok(response);
+    }
+
 }

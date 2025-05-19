a0fe9f6b7b5ed3a974f9cf7f1b2ce8cfc7b86ebb
dev-Chaves <chavesprogrammer@gmail.com>
2025-05-18 23:43:02 -0300

    data sql adjustment

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,2 @@
-INSERT INTO tb_enterprise (id, enterprise, cnpj, created_at)
-VALUES (1, 'Empresa Padrão', '12345678000100', now());
-
-INSERT INTO tb_user (id, name, password, enterprise_id, role, created_at)
-VALUES (1, 'admin', 'admin', 1, 'ADMIN', now());
+INSERT INTO tb_enterprise (name, cnpj, created_at)
+VALUES ('Empresa Padrão', '12345678000100', now());

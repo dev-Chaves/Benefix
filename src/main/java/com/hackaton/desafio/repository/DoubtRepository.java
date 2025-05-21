b0c58363dff3a2f64bce3c943919cf4d62a773a9
chaves <chavesprogrammer@gmail.com>
2025-05-21 09:33:21 -0300

    doubt repository

--- src/main/java/com/hackaton/desafio/repository/DoubtRepository.java
+++ src/main/java/com/hackaton/desafio/repository/DoubtRepository.java
@@ -1,4 +1,7 @@
 package com.hackaton.desafio.repository;
 
-public interface DoubtRepository {
+import com.hackaton.desafio.entity.IA.DoubtEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DoubtRepository extends JpaRepository<DoubtEntity, Long> {
 }

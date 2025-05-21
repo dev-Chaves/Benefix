aee5f7169c5bd92561a824a6b87ece2fb1df2618
chaves <chavesprogrammer@gmail.com>
2025-05-21 09:33:00 -0300

    endpoint for romulo

--- src/main/java/com/hackaton/desafio/controller/IA/IAController.java
+++ src/main/java/com/hackaton/desafio/controller/IA/IAController.java
@@ -1,4 +1,31 @@
 package com.hackaton.desafio.controller.IA;
 
+import com.hackaton.desafio.dto.IA.DoubtRequest;
+import com.hackaton.desafio.dto.IA.DoubtResponse;
+import com.hackaton.desafio.services.UserService;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("ia")
 public class IAController {
+
+    private final UserService userService;
+
+    public IAController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("doubt")
+    public ResponseEntity<DoubtResponse> createDoubt(@Valid @RequestBody DoubtRequest doubt){
+
+        return userService.createDoubt(doubt);
+
+    }
+
 }

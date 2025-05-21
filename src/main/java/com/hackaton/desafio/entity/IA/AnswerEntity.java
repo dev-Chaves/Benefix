432458417dcf13e3b0b3d1ee91cc551a7b5860ee
chaves <chavesprogrammer@gmail.com>
2025-05-21 09:32:46 -0300

    create Entity for AI

--- src/main/java/com/hackaton/desafio/entity/IA/AnswerEntity.java
+++ src/main/java/com/hackaton/desafio/entity/IA/AnswerEntity.java
@@ -1,4 +1,24 @@
 package com.hackaton.desafio.entity.IA;
 
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "tb_answer")
 public class AnswerEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "doubt_id")
+    private DoubtEntity doubt;
+
+    private String response;
+
+    private LocalDateTime createdAt;
+
+
 }
--- src/main/java/com/hackaton/desafio/entity/IA/DoubtEntity.java
+++ src/main/java/com/hackaton/desafio/entity/IA/DoubtEntity.java
@@ -1,8 +1,8 @@
-package com.hackaton.desafio.entity;
+package com.hackaton.desafio.entity.IA;
 
+import com.hackaton.desafio.entity.UserEntity;
 import jakarta.persistence.*;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Entity
@@ -17,8 +17,60 @@ public class DoubtEntity {
 
     private Boolean answered = false;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private UserEntity user;
+
     private LocalDateTime createdAt;
-    
 
+    @OneToOne(mappedBy = "doubt", cascade = CascadeType.ALL)
+    private AnswerEntity answer;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public Boolean getAnswered() {
+        return answered;
+    }
+
+    public void setAnswered(Boolean answered) {
+        this.answered = answered;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public AnswerEntity getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(AnswerEntity answer) {
+        this.answer = answer;
+    }
+
+    public UserEntity getUser() {
+        return user;
+    }
+
+    public void setUser(UserEntity user) {
+        this.user = user;
+    }
 }

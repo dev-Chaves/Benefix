65958f9447e52dd331bf7bd67893648ceee18d0c
chaves <chavesprogrammer@gmail.com>
2025-05-22 14:49:11 -0300

    IA doc

--- src/main/java/com/hackaton/desafio/dto/IA/DoubtResponse.java
+++ src/main/java/com/hackaton/desafio/dto/IA/DoubtResponse.java
@@ -1,9 +1,14 @@
 package com.hackaton.desafio.dto.IA;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 public record DoubtResponse(
 
+        @Schema(description = "The question to be asked", example = "How many meetings do I have today?")
         String doubt,
+        @Schema(description = "User who asked the question", example ="João")
         String user,
+        @Schema(description = "The answer to the question", example = "You have 3 meetings today")
         Boolean answered
 
 

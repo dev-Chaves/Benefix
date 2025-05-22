d534f5edf4fc5882a68d50f0ad92f375c6f226c9
chaves <chavesprogrammer@gmail.com>
2025-05-22 14:54:59 -0300

    user docs

--- src/main/java/com/hackaton/desafio/dto/userDTO/UserRequest.java
+++ src/main/java/com/hackaton/desafio/dto/userDTO/UserRequest.java
@@ -1,11 +1,16 @@
 package com.hackaton.desafio.dto.userDTO;
 
 import com.hackaton.desafio.entity.Role.Role;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 public record UserRequest(
+        @Schema(description = "User's name", example = "John Doe")
         String name,
+        @Schema(description = "User's password", example = "password123")
         String password,
+        @Schema(description = "Enterprise ID of the user", example = "1")
         Long enterprise,
+        @Schema(description = "User's role", example = "USER")
         Role role
-        ) {
+) {
 }
--- src/main/java/com/hackaton/desafio/dto/userDTO/UserResponse.java
+++ src/main/java/com/hackaton/desafio/dto/userDTO/UserResponse.java
@@ -1,10 +1,14 @@
 package com.hackaton.desafio.dto.userDTO;
 
 import com.hackaton.desafio.entity.Role.Role;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 public record UserResponse(
+        @Schema(description = "User's name", example = "John Doe")
         String name,
+        @Schema(description = "User's role", example = "USER")
         Role role,
+        @Schema(description = "Name of the enterprise", example = "Acme Corp")
         String empresa
 ) {
 }

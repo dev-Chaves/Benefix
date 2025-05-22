c1a013f3176165e9f7b452d8daa6640161e9dd93
chaves <chavesprogrammer@gmail.com>
2025-05-22 14:15:48 -0300

    enterprise documentation

--- src/main/java/com/hackaton/desafio/dto/enterpriseDTO/EnterpriseRequest.java
+++ src/main/java/com/hackaton/desafio/dto/enterpriseDTO/EnterpriseRequest.java
@@ -1,7 +1,11 @@
 package com.hackaton.desafio.dto.enterpriseDTO;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 public record EnterpriseRequest(
+        @Schema(description = "Enterprise name", example = "Benefix")
         String name,
+        @Schema(description = "Enterprise CNPJ", example = "12345678000195")
         String cnpj
 ) {
 }
--- src/main/java/com/hackaton/desafio/dto/enterpriseDTO/EnterpriseResponse.java
+++ src/main/java/com/hackaton/desafio/dto/enterpriseDTO/EnterpriseResponse.java
@@ -1,8 +1,13 @@
 package com.hackaton.desafio.dto.enterpriseDTO;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 public record EnterpriseResponse (
+        @Schema(description = "Enterprise ID", example = "1")
         Long id,
+        @Schema(description = "Enterprise name", example = "Benefix")
         String name,
+        @Schema(description = "Enterprise CNPJ", example = "12345678000195")
         String cnpj
 ){
 }

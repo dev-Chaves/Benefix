556d5f2e64426c686e2e21a49bfd5d7a01313a22
chaves <chavesprogrammer@gmail.com>
2025-05-22 14:52:16 -0300

    partnership doc

--- src/main/java/com/hackaton/desafio/dto/partnershipDTO/PartnershipRequest.java
+++ src/main/java/com/hackaton/desafio/dto/partnershipDTO/PartnershipRequest.java
@@ -1,7 +1,11 @@
 package com.hackaton.desafio.dto.partnershipDTO;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 public record PartnershipRequest(
+        @Schema(description = "Id of  supplier enterprise", example = "1")
         Long supplierEnterpriseId,
+        @Schema(description = "Id of consumer enterprise", example = "2")
         Long consumerEnterpriseId
 ) {
 }

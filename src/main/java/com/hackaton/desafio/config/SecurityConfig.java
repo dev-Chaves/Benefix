e1248ca2eb179aa46a449b0ecb1c322842933e7f
dev-Chaves <chavesprogrammer@gmail.com>
2025-06-21 22:01:18 -0300

    aumentando quantidade limite de conexões no pool do db

--- src/main/java/com/hackaton/desafio/config/SecurityConfig.java
+++ src/main/java/com/hackaton/desafio/config/SecurityConfig.java
@@ -50,7 +50,7 @@ public class SecurityConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder(){
-        return new BCryptPasswordEncoder();
+        return new BCryptPasswordEncoder(8);
     }
 
     @Bean
